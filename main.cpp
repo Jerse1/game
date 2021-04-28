6b3eb4e669a7702f6bd9d2b26b2a75033038ea77
Jerse1 <emilis.narbutas1@gmail.com>
2021-04-28 22:10:29 +0300

    Basic drawing system

--- main.cpp
+++ main.cpp
@@ -3,26 +3,62 @@
 #else
 #include <unistd.h>
 #endif
-
-#include <stdlib.h>
 #include <iostream>
+#include <string>
 
-#define SCREEN_HEIGHT 720;
-#define SCREEN_LENGTH 1080;
+const int SCREEN_HEIGHT = 25;
+const int SCREEN_WIDTH = 80;
+
+bool GAME_ON = 1;
+
+int Position[2] = {40, 5};  // x,y;
+int Direction[2] = {0, -1}; // x,y;
+
+void Draw()
+{
+    system("CLS");
+    for (int i = 0; i < 2; i++)
+    {
+        if (Direction[i] != 0)
+            Position[i] += Direction[i];
+        if (Position[0] == SCREEN_WIDTH - 1 && Direction[0] == 1)
+            Position[0] = 1;
+        else if (Position[0] == 1 && Direction[0] == -1)
+            Position[0] = SCREEN_WIDTH;
+        else if (Position[1] == SCREEN_HEIGHT - 1 && Direction[1] == 1)
+            Position[1] = 1;
+        else if (Position[1] == 1 && Direction[1] == -1)
+            Position[1] = SCREEN_HEIGHT;
+    }
+
+    for (int y = 0; y < SCREEN_HEIGHT; y++)
+    {
+        std::cout << "X";
+        for (int x = 0; x < SCREEN_WIDTH; x++)
+        {
+            if (y == 0 || y == SCREEN_HEIGHT - 1)
+                std::cout << "X";
+            else if (x == Position[0] && y == Position[1])
+                std::cout << "@";
+            else
+                std::cout << " ";
+        }
+        std::cout << "X\n";
+    }
+}
+
+void Start()
+{
+    while (GAME_ON == 1)
+    {
+        Draw();
+        Sleep(100);
+    }
+}
 
 int main()
 {
-
-
-    std::cout << "wait" << std::endl;
-    Sleep(1);
-    std::cout << "begin" << std::endl;
-    for (int y = 1; y < 720; y++)
-    {
-        for (int x = 1; x < 1080; x++)
-        {
-            std::cout << "*";
-        }
-    }
+    Start();
+    std::cin.get();
     return 0;
 }
